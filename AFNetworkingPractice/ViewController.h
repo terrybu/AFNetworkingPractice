9c058269adae0d4af2036cbd39889bdd34212ab0
Terry Bu <ktbucifer@gmail.com>
2014-12-12 12:01:13 -0500

    first

---
+++ AFNetworkingPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingPractice
+//
+//  Created by Aditya Narayan on 12/12/14.
+//  Copyright (c) 2014 TerryBuOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+inhibit_all_warnings!
+
+
+# Uncomment this line to define a global platform for your project
+platform :ios, '7.0'
+
+target 'AFNetworkingPractice' do
+
+pod "AFNetworking", "~> 2.0"
+
+end
+
+target 'AFNetworkingPracticeTests' do
+
+end
+
